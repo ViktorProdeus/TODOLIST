afc05ad8e63e0d72ddf0c7b65a2028a5652a4e26
ViktorProdeus <viktorprodeus@gmail.com>
2021-06-13 18:31:09 +0300

    Updates

---
+++ precache-manifest.75ff1d894d694137825462d25f77d270.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "41502161459b6ecf99c71bedd4ac007c",
+    "url": "/todolist/index.html"
+  },
+  {
+    "revision": "356c116909c6f4a2f11a",
+    "url": "/todolist/static/css/main.2a7eaf4e.chunk.css"
+  },
+  {
+    "revision": "f543ebb13449f015479b",
+    "url": "/todolist/static/js/2.c4a9203b.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/todolist/static/js/2.c4a9203b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "356c116909c6f4a2f11a",
+    "url": "/todolist/static/js/main.1b22b042.chunk.js"
+  },
+  {
+    "revision": "471e54e937a3aef18e16",
+    "url": "/todolist/static/js/runtime-main.283d8678.js"
+  }
+]);
